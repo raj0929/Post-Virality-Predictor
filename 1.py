5899a9ee60f2d2a8e46294af83ddc13b6ac9ee09
raj0929 <142717255+raj0929@users.noreply.github.com>
2025-04-05 11:58:30 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -106,15 +106,19 @@ if menu_tabs:
             st.plotly_chart(virality_status, use_container_width=True)
 
         st.subheader("🏆 Top 10 Posts with Highest Virality Score")
-        data = pd.read_csv(uploaded_file, usecols=['meme_id','text_content_type','platform','likes','shares','comments','virality_score'])
-        data_sorted = data.sort_values(by='virality_score',ascending=False).head(10)
-        write(data_sorted)
+        top_posts = df.nlargest(10, 'virality_score')[
+                ['meme_id', 'text_content_type', 'platform', 'virality_score']]
+        st.dataframe(
+            top_posts,
+            use_container_width=True
+        )
 
         st.subheader("📊 Average Engagement by Platform")
-        data1 = pd.read_csv(uploaded_file,
-                           usecols=['platform', 'likes', 'shares', 'comments','virality_score'])
-        platform_avg = data1.groupby('platform')[['likes','shares','comments','virality_score']].mean()
-        write(platform_avg)
+        platform_avg = df.groupby('platform')[['likes', 'shares', 'comments', 'virality_score']].mean()
+        st.dataframe(
+            platform_avg,
+            use_container_width=True
+        )
 
     if menu_tabs == 'Predictions':
         st.header("🔮 Virality Predictor")
